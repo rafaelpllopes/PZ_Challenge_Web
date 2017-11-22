ea749b3e444c67dfa94ef1bc50f3883b2a1aea16
rafaelpllopes <rafelpllopes@gmail.com>
2017-11-22 19:54:36 -0200

    configurando as rotas.

--- app-pz-challenge/src/app/app.routes.ts
+++ app-pz-challenge/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ListaComponent } from './lista/lista.component';
+import { VideoComponent } from './video/video.component';
 
 const appRoutes: Routes = [
     { path: '', component: ListaComponent },
+    { path: 'video/:id', component: VideoComponent },
     { path: '**', component: ListaComponent }
 ];
 
--- app-pz-challenge/src/app/video/video.component.ts
+++ app-pz-challenge/src/app/video/video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-video',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponent implements OnInit {
 
-  constructor() { }
+  private id;
+  private route;
+  private router;
+  private video;
+
+  constructor(route: ActivatedRoute, router: Router) {
+    this.route = route;
+    this.router = router;
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      //this.video = this.id;
+    });
+  }
 
   ngOnInit() {
   }
