3c127b65928cc81604c90776e1ee1950b35903a5
rafaelpllopes <rafelpllopes@gmail.com>
2017-11-23 15:59:26 -0200

    Ajute na url e novo build

--- app-pz-challenge/src/app/services/json-service.ts
+++ app-pz-challenge/src/app/services/json-service.ts
@@ -9,10 +9,14 @@ export class JsonService {
     addVideos = new EventEmitter();
     //variavel que armazena a url que possui o json
     //private url: string = 'https://pbmedia.pepblast.com/pz_challenge/assets.json';
-    private url: string = 'http://localhost:3000/json';
+    private url: string = `http://${this.host}:3000/json`;
 
     constructor(private http: Http) {
         this.atualiza();
+    }
+
+    get host() {
+        return location.hostname;
     }
 
     //Metodo para adcionar os itens obtidos pelo Json
